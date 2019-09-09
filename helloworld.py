cd6c05fe88f70b58f735c44be2c3e98e35f772cd
Ethan Mook <emook@umich.edu>
2019-09-09 16:15:59 -0400

    Added the random print statements like said in the instructions to this lab

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import random
+
+print("Ethan Mook")
+a = random.randrange(100)
+b = random.randrange(100)
+print(a)
+print(b)
+print("Sum = {}".format(a + b))
+print("Average = {}".format((a + b) / 2.0))
